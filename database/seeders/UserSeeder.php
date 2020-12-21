a2b8e56f388ddaa5b7a8df83b1a8184b7506876f
Aditya Permadi <56598947+addityap06@users.noreply.github.com>
2020-12-21 15:00:12 +0700

    Menambahkan User seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,12 +17,21 @@ class UserSeeder extends Seeder
     {
         //
         User::truncate();
-        User::create([
-            'name' => 'Admin Aplikasi',
-            'level' => 'admin',
-            'email' => 'admin@mail.com',
-            'password' => bcrypt('12345'),
-            'remember_token' => Str::random('20')
+        User::insert([
+            [
+                'name' => 'Admin Aplikasi',
+                'level' => 'admin',
+                'email' => 'admin@mail.com',
+                'password' => bcrypt('12345'),
+                'remember_token' => Str::random('20')
+            ],
+            [
+                'name' => 'User Aplikasi',
+                'level' => 'pengguna',
+                'email' => 'user@mail.com',
+                'password' => bcrypt('12345'),
+                'remember_token' => Str::random('20')
+            ],
         ]);
     }
 }
